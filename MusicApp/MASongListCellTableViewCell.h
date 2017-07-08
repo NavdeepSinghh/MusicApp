cbdc93d17fc5a0ea14629a9f6cc0462275c7592b
Navdeep  Singh <rudrasambyal@gmail.com>
2017-07-08 19:20:17 +1000

    UiTableViewCell Implemented

---
+++ MusicApp/MASongListCellTableViewCell.h
@@ -0,0 +1,24 @@
+//
+//  MASongListCellTableViewCell.h
+//  MusicApp
+//
+//  Created by Navdeep Singh on 8/07/17.
+//  Copyright © 2017 Navdeep Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MAModel.h"
+#import "FLAnimatedImageView.h"
+#import "FLAnimatedImage.h"
+
+@interface MASongListCellTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet FLAnimatedImageView *imgArtistArt;
+@property (weak, nonatomic) IBOutlet UILabel *lblSongName;
+@property (weak, nonatomic) IBOutlet UILabel *lblArtistname;
+@property (weak, nonatomic) IBOutlet UILabel *lblArtistAlbum;
+@property (weak, nonatomic) IBOutlet UIImageView *imgPlayIndicator;
+
+
+-(void)cellWithData:(MAModel *)songObject isPlaying:(BOOL)isPlaying;
+
+@end
---
+++ MusicApp/MASongListCellTableViewCell.m
@@ -0,0 +1,25 @@
+//
+//  MASongListCellTableViewCell.m
+//  MusicApp
+//
+//  Created by Navdeep Singh on 8/07/17.
+//  Copyright © 2017 Navdeep Singh. All rights reserved.
+//
+
+#import "MASongListCellTableViewCell.h"
+
+@implementation MASongListCellTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+
+@end
